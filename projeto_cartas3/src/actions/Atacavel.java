8d0bf80fd9a2d297443bf09615cf055672d94507
Arthur Pontes Ferreira <apf3@poli.br>
2024-11-26 02:06:34 -0300

    Delete projeto_cartas3 directory

--- projeto_cartas3/src/actions/Atacavel.java
+++
@@ -1,8 +0,0 @@
-package actions;
-
-import model.Criatura;
-import model.Jogador;
-
-public interface Atacavel {
-    //public void atacar(Jogador jogadorA, Jogador jogadorD, Criatura atacante, Criatura defensor);
-}
--- projeto_cartas3/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import controller.Jogo;
-
-public class Main {
-    public static void main(String[] args) {
-        Jogo.inciarJogo();
-    }
-}
